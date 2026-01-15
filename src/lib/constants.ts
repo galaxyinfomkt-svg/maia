8a9ba4a6624ff074a9b4003e3db9af8aa0c19f0f
galaxyinfomkt-svg <galaxyinfomkt@gmail.com>
2026-01-14 23:41:51 -0300

    3

---
+++ src/lib/constants.ts
@@ -0,0 +1,37 @@
+export const SITE_NAME = 'Maia Construction';
+export const SITE_URL = 'https://maiaconstruction.com';
+export const PHONE = '(508) 859-9880';
+export const PHONE_LINK = 'tel:+15088599880';
+
+export const ADDRESS = {
+  street: '123 Main Street',
+  city: 'Marlborough',
+  state: 'MA',
+  zip: '01752',
+  full: 'Marlborough, MA 01752',
+  lat: 42.3459,
+  lng: -71.5526,
+};
+
+export const IMAGES = {
+  hero: 'https://storage.googleapis.com/msgsndr/b8spY0hvhFRzHHxsdJlZ/media/68bb92a147f24d34a4cb2441.jpeg',
+  siding: 'https://storage.googleapis.com/msgsndr/b8spY0hvhFRzHHxsdJlZ/media/68b9ef0b12b656a7091a9bab.jpeg',
+  windows: 'https://storage.googleapis.com/msgsndr/b8spY0hvhFRzHHxsdJlZ/media/68b9ee4857df8a993dff5806.jpeg',
+  wood: 'https://storage.googleapis.com/msgsndr/b8spY0hvhFRzHHxsdJlZ/media/68bb8b0cb20028428edc255a.jpeg',
+  engineered: 'https://storage.googleapis.com/msgsndr/b8spY0hvhFRzHHxsdJlZ/media/68bb8b8e52c9caae94da8dd1.jpeg',
+  logo: 'https://storage.googleapis.com/msgsndr/b8spY0hvhFRzHHxsdJlZ/media/69165fb4890fad61b2da1dc3.png',
+};
+
+export const LOGO_URL = IMAGES.logo;
+
+export const SOCIAL_LINKS = {
+  facebook: '#',
+  instagram: '#',
+  linkedin: '#',
+};
+
+export const BUSINESS_HOURS = {
+  weekdays: '8:00 AM - 6:00 PM',
+  saturday: '9:00 AM - 3:00 PM',
+  sunday: 'Closed',
+};
