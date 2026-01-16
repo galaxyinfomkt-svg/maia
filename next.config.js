8c874e470c9020c498f3f62b85d4f030334eeea2
galaxyinfomkt-svg <galaxyinfomkt@gmail.com>
2026-01-15 21:19:31 -0300

    9

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.leadconnectorhq.com",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
